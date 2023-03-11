6c4d8b30e6428785ac8613d962af2924cb8fdb04
Toby16 <bolajibaasit14@gmail.com>
2023-03-11 10:06:18 -0500

    state.py | More Classes | fixed pycodestyle error | made file executable

---
+++ models/state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+'State' class
+"""
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """
+    class State inherited form Parent class 'BaseModel'
+
+    Public class attrubute:
+        name: Name of the state
+    """
+    name = ""
